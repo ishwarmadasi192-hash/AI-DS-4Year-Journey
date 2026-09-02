ae89c66b84ea9cec4c0d2923759272bdb2e6a533
Ishwar <ishwarmadasi192@gmail.com>
2026-09-02 14:11:09 +0530

    Add day1_test.py: basic input/output and simple calculations

---
+++ day1_test.py
@@ -0,0 +1,9 @@
+#first test
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+print(f"Hello, {name}!")
+print(f"You are {age} years old.")
+birth_year = 2026 - int(age)
+year_turn_21 = birth_year + 21
+print(f" Youe= were born in year {birth_year}.")
+print(f"You will turn 21 in the year {year_turn_21}.")
